c6cc8d675e8514945005d6c0a1f3418b4e34b88d
Kenneth Lee <leechinhongkenneth@gmail.com>
2020-07-01 02:22:52 -0700

    added medium

---
+++ Medium/smallestDifference.py
@@ -0,0 +1,28 @@
+# Write a function tha takes two non-empty arrays and return two elements that 
+# the absolute different between them is closest to zero
+
+def smallestDifference(arrayOne, arrayTwo):
+    arrayOne.sort()
+	arrayTwo.sort()
+	idxOne = 0
+	idxTwo = 0
+	smallest = float("inf")
+	current = float("inf")
+	smallestPair = []
+	while idxOne < len(arrayOne) and idxTwo < len(arrayTwo):
+		firstNum = arrayOne[idxOne]
+		secondNum = arrayTwo[idxTwo]
+		if firstNum < secondNum:
+			current = secondNum - firstNum
+			idxOne += 1
+		elif secondNum < firstNum:
+			current = firstNum - secondNum
+			idxTwo += 1
+		else:
+			return [firstNum, secondNum]
+		if smallest > current:
+			smallest = current
+			smallestPair = [firstNum, secondNum]
+	return smallestPair
+	
+    
